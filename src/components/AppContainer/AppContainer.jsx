c87115e7bbf1d6c4336c1fd38d125efcc12009f0
Endrius <endriussilvasantos@gmail.com>
2025-10-07 20:13:30 -0300

    removendo as safe areas do Safari

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -1,6 +1,6 @@
 const AppContainer = ({ children }) => {
   return (
-    <div className="flex min-h-screen flex-col bg-gray-950 font-[Poppins]">
+    <div className="pt-safe-top pb-safe-bottom flex min-h-screen flex-col bg-gray-950 font-[Poppins]">
       {children}
     </div>
   );
