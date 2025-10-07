5459c4833c10711a94e84f5d7ef1a068670c32ac
Endrius <endriussilvasantos@gmail.com>
2025-10-07 20:17:45 -0300

    atualizando estilos para que seja removido as bordas brancas do navegador Safari

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -1,6 +1,6 @@
 const AppContainer = ({ children }) => {
   return (
-    <div className="pt-safe-top pb-safe-bottom flex min-h-screen flex-col bg-gray-950 font-[Poppins]">
+    <div className="pt-safe-top pb-safe-bottom flex min-h-[100dvh] flex-col bg-gray-950 font-[Poppins]">
       {children}
     </div>
   );
