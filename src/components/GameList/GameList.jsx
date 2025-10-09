fbafef9f43703b0969be7e8d68bd9046d6e3159c
Endrius <endriussilvasantos@gmail.com>
2025-10-09 18:53:07 -0300

    criado botão para ver detalhes de um jogo

--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -8,28 +8,50 @@ const GameList = () => {
     staleTime: 1000 * 60 * 5,
   });
 
-  if (isLoading) return <div className="p-6">Carregando jogos...</div>;
+  if (isLoading)
+    return (
+      <div className="pt-100 text-center text-white">Carregando jogos...</div>
+    );
   if (isError)
-    return <div className="p-6 text-red-400">Erro: {error.message}</div>;
+    return (
+      <div className="pt-100 text-center text-red-400">
+        Erro: {error.message}
+      </div>
+    );
 
   const games = data?.results || [];
 
   return (
     <div className="p-6">
-      <h2 className="mb-4 text-xl font-semibold">Jogos (com React Query)</h2>
+      <h2 className="mb-4 text-xl font-semibold text-white">Top Jogos</h2>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
         {games.map((g) => (
           <div key={g.id} className="overflow-hidden rounded bg-slate-800 p-2">
             <img
               src={g.background_image}
               alt={g.name}
-              className="h-40 w-full rounded object-cover"
+              className="h-60 w-full rounded object-cover"
             />
-            <div className="mt-2">
-              <h3 className="text-sm font-medium">{g.name}</h3>
-              <p className="text-xs text-gray-300">
-                Nota: {g.rating} / {g.ratings_count}
-              </p>
+            <div className="flex items-center justify-between p-3">
+              <div className="mt-2">
+                <h3 className="text-sm font-medium text-white">{g.name}</h3>
+                <p
+                  className={`text-xs font-semibold ${
+                    g.metacritic >= 75
+                      ? "text-green-400"
+                      : g.metacritic >= 50
+                        ? "text-yellow-400"
+                        : "text-red-400"
+                  }`}
+                >
+                  Metacritic: {g.metacritic ? `${g.metacritic}/100` : "N/A"}
+                </p>
+              </div>
+              <div>
+                <button className="cursor-pointer rounded-xl px-3 py-2 text-white transition-all duration-300 hover:bg-blue-900">
+                  Ver detalhes
+                </button>
+              </div>
             </div>
           </div>
         ))}
