e3001777dcd899b4bbd5b391c4b87b11f659c864
Endrius <endriussilvasantos@gmail.com>
2025-10-13 21:27:34 -0300

    atualizado textos de About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,29 @@
+import { Github } from "lucide-react";
+
 const About = () => {
   return (
-    <div className="m-auto flex items-center justify-center p-5 text-center text-white lg:max-w-1/2">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa pariatur
-      quae ducimus perferendis similique maxime fugit tempora deleniti, officiis
-      nam et porro, saepe neque consequatur ea dolorum deserunt sint doloremque!
+    <div className="m-auto flex flex-col items-center justify-center gap-2 p-5 text-center text-white lg:max-w-1/2">
+      <p>
+        O GameTrack é uma aplicação feita para gamers que buscam descobrir novos
+        jogos, ver notas, avaliações e informações detalhadas sobre cada título.
+      </p>
+      <p>
+        Foi desenvolvida com React, Tailwind CSS e React Query, consumindo a API
+        RAWG.io para exibir dados atualizados sobre os principais lançamentos.
+      </p>
+      <p>
+        O objetivo do projeto é praticar conceitos modernos do ecossistema React
+        e, ao mesmo tempo, criar uma experiência agradável e útil para quem ama
+        videogames.
+      </p>
+      <p>
+        💡 Em breve, o GameTrack contará com cadastro de usuários, listas
+        personalizadas e muito mais!
+      </p>
+      <p>👨🏻‍💻 Criado por Endrius</p>
+      <a href="https://github.com/endriusssantos" target="_blank">
+        <Github />
+      </a>
     </div>
   );
 };
